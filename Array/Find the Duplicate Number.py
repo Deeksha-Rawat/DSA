b349e4abed5833486cd533da034b3d74b9274eb4
Deeksha Rawat <50552823+Deeksha-Rawat@users.noreply.github.com>
2021-09-12 22:35:21 +0530

    Update Find the Duplicate Number.py

--- Array/Find the Duplicate Number.py
+++ Array/Find the Duplicate Number.py
@@ -1,4 +1,4 @@
-
+###solution1###
 
 a=[2,2,1,4,3]
 n =len(a)
@@ -8,3 +8,12 @@ for i in range(1,n):
     if a[i]==a[i-1]:
         count+=1
         print(a[i])    
+
+        
+        
+###solution2(more optimized)###
+
+a=[2,2,1,4,3]
+b = Counter(a)
+res = b.most_common()[0][0]
+print(res)
