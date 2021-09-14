830494fd26497ae27f5bc2e28b2f88d8f2d8f19d
Deeksha Rawat <50552823+Deeksha-Rawat@users.noreply.github.com>
2021-09-14 20:28:03 +0530

    Update Find first and last positions of an element in a sorted array.py

--- Searching and Sorting/Find first and last positions of an element in a sorted array.py
+++ Searching and Sorting/Find first and last positions of an element in a sorted array.py
@@ -16,3 +16,44 @@ else:
 ######using binary search######
   
   
+
+             
+""" first  """
+def first(a,x,n):
+    low=0
+    high=n-1
+    res =-1
+    while(low<=high):
+        mid = (low+high)//2
+        if a[mid]>x:
+            high=mid-1
+        elif a[mid]<x:
+            low=mid+1
+        else:
+            res = mid
+            high =mid-1
+    return res
+
+def last(a,x,n):
+    low=0
+    high=n-1
+    res=-1
+    while(low<=high):
+        mid=(low+high)//2
+        if a[mid]>x:
+            high=mid-1
+        elif a[mid]<x:
+            low=mid+1
+        else:
+            res=mid
+            low=mid+1
+    return res
+
+a=[1, 3, 5, 5, 5, 5, 67, 123, 125 ]
+n=9
+x=5
+print(first(a,x,n),last(a,x,n))                    
+
+
+
+
