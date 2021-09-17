9b044e678ec1897e84c6096e05cbf962fd65549a
Deeksha Rawat <50552823+Deeksha-Rawat@users.noreply.github.com>
2021-09-18 00:03:02 +0530

    Create Write a Program to reverse the Linked List. (Both Iterative and recursive).py

---
+++ Searching and Sorting/Write a Program to reverse the Linked List. (Both Iterative and recursive).py
@@ -0,0 +1,42 @@
+##Iterative Method
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def reverse(self):
+        prev =None
+        current =self.head
+        while(current is not None):
+            temp = current.next
+            current.next = prev
+            prev = current
+            current = temp       
+
+        self.head = prev       
+
+    def insert(self,new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+llist = LinkedList()
+llist.insert(20)                     
+llist.insert(40)                                        
+llist.insert(320)                     
+llist.insert(520)                     
+llist.insert(270)                     
+llist.insert(220) 
+llist.printList()                    
+llist.reverse()
+llist.printList()
