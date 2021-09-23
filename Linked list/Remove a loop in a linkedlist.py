71041ce59014dfce3f83bbe0202e75999632e311
Deeksha Rawat <50552823+Deeksha-Rawat@users.noreply.github.com>
2021-09-23 12:45:18 +0530

    Update Remove a loop in a linkedlist.py

--- Linked list/Remove a loop in a linkedlist.py
+++ Linked list/Remove a loop in a linkedlist.py
@@ -1,39 +1,4 @@
-def detectloop(self):
-    slow = fast = self.head
-    while(slow and fast and fast.next):
-        slow = slow.next
-        fast = fast.next.next
-        if slow == fast:
-            self.removeloop(slow)
-            return 1
-    return 0
-
-def removeloop(self,loopnode):
-    ptr1 = loopnode
-    ptr2 = loopnode
-
-    k=1
-    while(ptr1.next!=ptr2):
-        k+=1
-        ptr1=ptr1.next
-
-    ptr1 = self.head
-    ptr2 = self.head
-    for i in range(k):
-        ptr2 = ptr2.next
-    while(ptr2!=ptr1):
-        ptr1 = ptr1.next        
-        ptr2 = ptr2.next 
-
-    while(ptr2.next!=ptr1):
-        ptr2 = ptr2.next
-
-    ptr2.next=None
-                   
-
-        
-        
-        
+ 
  ###using hashing####
 
 def removeLoop(self, head):
@@ -46,7 +11,6 @@ def removeLoop(self, head):
         h=set()
         while(p1):
             if p1 in h:
-                
                 prev.next = None
                 return
             h.add(p1)
