6e519b9283d032788eded3241a0e0f367971cdf2
Deeksha Rawat <50552823+Deeksha-Rawat@users.noreply.github.com>
2021-09-23 14:26:28 +0530

    Create Remove duplicates from an unsorted linked list.py

---
+++ Linked list/Remove duplicates from an unsorted linked list.py
@@ -0,0 +1,15 @@
+def removeduplicates(self,head):
+  if head==None or head.next==None:
+    return head
+  current = head
+  hash=set()
+  hash.add(head.data)
+  current =head
+  while(current):
+    if current.next.data in hash:
+      current.next=current.next.next
+    else:
+      hash.add(current.next.data)
+      current=current.next
+  return head    
+  
