c9e7989a50777ecf5ef90be9d7ad3055123d3753
Deeksha Rawat <50552823+Deeksha-Rawat@users.noreply.github.com>
2021-10-01 19:31:07 +0530

    Update check for bst.py

--- Binary Search Tree/check for bst.py
+++ Binary Search Tree/check for bst.py
@@ -9,3 +9,18 @@ def isBST(root,min_allowed,max_allowed):
         leftvalid = isBST(root.left,min_allowed,root)
         rightvalid = isBST(root.right,root,max_allowed) 
         return leftvalid and rightvalid 
+
+
+
+#geeks for geeks practice solution#######333
+def isBST(self,root,min_allowed=None,max_allowed=None):
+        if root is None:
+            return True
+        if(min_allowed is not None and root.data <= min_allowed.data):
+            return False
+        if(max_allowed is not None and root.data >= max_allowed.data):
+            return False
+    
+        leftvalid = self.isBST(root.left,min_allowed,root)
+        rightvalid = self.isBST(root.right,root,max_allowed) 
+        return leftvalid and rightvalid  
