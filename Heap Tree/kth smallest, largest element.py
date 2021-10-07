5dbdc4ffc01ad4cdf3b6a3366116f1595d146d83
Deeksha Rawat <50552823+Deeksha-Rawat@users.noreply.github.com>
2021-10-07 13:08:10 +0530

    Update kth smallest, largest element.py

--- Heap Tree/kth smallest, largest element.py
+++ Heap Tree/kth smallest, largest element.py
@@ -10,3 +10,14 @@ class Solution:
       
 #for smallest
 ans =nsmallest(k,nums)
+
+
+##using priority queue
+class Solution:
+    def kthLargestNumber(self, nums: List[str], k: int) -> str:
+        heap =[-int(x) for x in nums]
+        for i in range(len(nums)):
+            heapify(heap)
+            for i in range(k-1):
+                heappop(heap)
+            return str(-heap[0]) 
